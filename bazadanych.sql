d77a01243fe758ca595db424d6c3be821b8e6760
Breadus1 <116367179+Breadus1@users.noreply.github.com>
2023-11-15 21:58:31 +0100

    Add files via upload

---
+++ bazadanych.sql
@@ -0,0 +1,34 @@
+CREATE TABLE IF NOT EXISTS tekst (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tresc TEXT
+);
+
+INSERT INTO tekst (tresc) VALUES ('Skład i struktura:');
+INSERT INTO tekst (tresc) VALUES ('Układ kostny składa się z 206 kości u dorosłego człowieka.');
+INSERT INTO tekst (tresc) VALUES ('Kości są zbudowane z tkanki kostnej, która zawiera komórki kostne, włókna kolagenowe i substancje mineralne.');
+
+INSERT INTO tekst (tresc) VALUES ('Funkcje układu kostnego:');
+INSERT INTO tekst (tresc) VALUES ('Wsparcie i kształt: Kości zapewniają strukturę i kształt ciału, wspierając inne tkanki.');
+INSERT INTO tekst (tresc) VALUES ('Ochrona: Chronią ważne organy, na przykład czaszka chroni mózg.');
+INSERT INTO tekst (tresc) VALUES ('Ruch: Kości działają jako dźwignie, umożliwiając ruch ciała.');
+INSERT INTO tekst (tresc) VALUES ('Produkcja komórek krwi: Szpik kostny jest miejscem produkcji komórek krwi.');
+INSERT INTO tekst (tresc) VALUES ('Magazynowanie minerałów: Kości magazynują minerały, takie jak wapń i fosfor.');
+
+INSERT INTO tekst (tresc) VALUES ('Typy kości:');
+INSERT INTO tekst (tresc) VALUES ('Długie kości: Ważne dla dużych ruchów i wsparcia.');
+INSERT INTO tekst (tresc) VALUES ('Krótkie kości: Zapewniające stabilność i pewną ruchomość.');
+INSERT INTO tekst (tresc) VALUES ('Płaskie kości: Chroniące wewnętrzne organy.');
+INSERT INTO tekst (tresc) VALUES ('Kości nieregularne: O złożonych kształtach, takie jak kręgi kręgosłupa.');
+
+INSERT INTO tekst (tresc) VALUES ('Zdrowie kości:');
+INSERT INTO tekst (tresc) VALUES ('Zdrowe kości wymagają odpowiedniej ilości wapnia, witaminy D i regularnych ćwiczeń.');
+INSERT INTO tekst (tresc) VALUES ('Osteoporoza to choroba osłabiająca kości.');
+
+INSERT INTO tekst (tresc) VALUES ('Rozwój i wzrost:');
+INSERT INTO tekst (tresc) VALUES ('Kości rosną i rozwijają się intensywnie w dzieciństwie i młodości.');
+INSERT INTO tekst (tresc) VALUES ('Gęstość kości osiąga szczyt w wieku dorosłym, a następnie może stopniowo maleć.');
+
+INSERT INTO tekst (tresc) VALUES ('Choroby i urazy:');
+INSERT INTO tekst (tresc) VALUES ('Układ kostny może być dotknięty różnymi schorzeniami, w tym złamaniami, infekcjami.');
+
+INSERT INTO tekst (tresc) VALUES ('Autor: Marcel Wątruch 2a');
